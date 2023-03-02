27effc32b2b0554cd0edf751667116faa2f15de2
hardikniranjani <hardikpniranjanice@gmail.com>
2023-03-02 19:22:16 +0530

    update baseUrl

--- Admin/src/app/Services/baseUrl.js
+++ Admin/src/app/Services/baseUrl.js
@@ -1,7 +1,11 @@
 function BaseUrl() {
-  if( process.env.REACT_APP_NEXT_PUBLIC_REST_API_ENDPOINT) return process.env.REACT_APP_NEXT_PUBLIC_REST_API_ENDPOINT
-  return "http://localhost:3001";
-  
+  // return "http://localhost:3001";
+  if (process.env.REACT_APP_NEXT_PUBLIC_REST_API_ENDPOINT) return process.env.REACT_APP_NEXT_PUBLIC_REST_API_ENDPOINT
+  return "https://themeforest.onrender.com/";
+
+
 }
 
 export default BaseUrl
+
+
